2a1a66d8b1ea07cf261dded6e518cc1e8f174e08
Nathan Yearout <nathan@payload.co>
2024-09-19 10:33:45 -0400

    Smooth scrolling

--- config.lua
+++ config.lua
@@ -95,6 +95,26 @@ lvim.plugins = {
       vim.g.mkdp_auto_start = 1
     end,
   },
+
+  -- Smooth Scrolling
+  {
+    "karb94/neoscroll.nvim",
+    event = "WinScrolled",
+    config = function()
+    require('neoscroll').setup({
+          mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
+          '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
+          hide_cursor = true,
+          stop_eof = true,
+          use_local_scrolloff = false,
+          respect_scrolloff = false,
+          cursor_scrolls_alone = true,
+          easing_function = nil,
+          pre_hook = nil,
+          post_hook = nil,
+          })
+    end
+  },
 }
 
 -- General Settings
